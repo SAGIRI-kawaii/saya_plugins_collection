500c1e834e092cfdb3088b35c4bd2567867bdab1
SAGIRI <shijiaduyifan@126.com>
2021-03-10 18:52:21 +0800

    添加了新的插件

--- README.md
+++ README.md
@@ -39,3 +39,4 @@
 - [WyySongOrderer](modules/WyySongOrderer) 一个(全损音质x)网易云源的点歌插件
 - [5000Zhao](modules/5000Zhao) 一个 5000兆円欲しい! style的图片生成器
 - [KeywordDetection](modules/KeywordDetection) 一个敏感词过滤插件（自带数据库）
+- [PhantomTank](modules/PhantomTank) 一个幻影坦克生成器
---
+++ modules/PhantomTank/__init__.py
@@ -0,0 +1,81 @@
+import aiohttp
+import os
+from io import BytesIO
+from PIL import Image as IMG
+
+from graia.saya import Saya, Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.application.exceptions import AccountMuted
+from graia.application import GraiaMiraiApplication
+from graia.application.event.messages import GroupMessage, Group, Member
+from graia.application.message.parser.kanata import Kanata
+from graia.application.message.parser.signature import RegexMatch
+from graia.application.event.messages import MessageChain
+from graia.application.message.elements.internal import Plain
+from graia.application.message.elements.internal import Image
+
+from .utils import make_tank
+
+# 插件信息
+__name__ = "PhantomTank"
+__description__ = "一个幻影坦克生成器"
+__author__ = "SAGIRI-kawaii"
+__usage__ = "群内发送 `幻影[图片][图片]` 即可"
+
+saya = Saya.current()
+channel = Channel.current()
+
+channel.name(__name__)
+channel.description(f"{__description__}\n使用方法：{__usage__}")
+channel.author(__author__)
+
+signal: int = 0
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def phantom_tank(app: GraiaMiraiApplication, message: MessageChain, group: Group):
+
+    if "".join([plain.text for plain in message.get(Plain)]).strip() == "幻影":
+        if len(message[Image]) != 2:
+            try:
+                await app.sendGroupMessage(group, MessageChain.create([Plain(text="非预期图片数量！请按 `显示图 隐藏图` 顺序发送，共两张")]))
+            except AccountMuted:
+                pass
+            return None
+        if globals()["signal"] >= 2:
+            try:
+                await app.sendGroupMessage(group, MessageChain.create([Plain(text=f"目前有{signal}个任务正在处理，请稍后再试！")]))
+            except AccountMuted:
+                pass
+            return None
+
+        globals()["signal"] += 1
+
+        try:
+            await app.sendGroupMessage(group, MessageChain.create([Plain(text="converting")]))
+        except AccountMuted:
+            pass
+
+        im_1 = message[Image][0]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=im_1.url) as resp:
+                img_content = await resp.read()
+        display_img = IMG.open(BytesIO(img_content))
+
+        im_2 = message[Image][1]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=im_2.url) as resp:
+                img_content = await resp.read()
+        hide_img = IMG.open(BytesIO(img_content))
+
+        try:
+            await app.sendGroupMessage(
+                group,
+                MessageChain.create([
+                    Image.fromUnsafeBytes(await make_tank(display_img, hide_img))
+                ])
+            )
+        except AccountMuted:
+            pass
+
+        globals()["signal"] -= 1
