3b18268c7e3ad2dcdf4562ff358898b1f2c18d48
Hato <63497854+Hato626@users.noreply.github.com>
2021-05-18 00:12:02 +0800

    Update Sqlite3Manager.py
    群词条的数据库路径多了个 '.'

--- modules/GroupWordCloudGenerator/Sqlite3Manager.py
+++ modules/GroupWordCloudGenerator/Sqlite3Manager.py
@@ -16,7 +16,7 @@ class Sqlite3Manager:
     def __init__(self):
         if not self.__first_init:
             self.path = os.getcwd()
-            self.__conn = sqlite3.connect(self.path + './modules/GroupWordCloudGenerator/chatRecord.db')
+            self.__conn = sqlite3.connect(self.path + '/modules/GroupWordCloudGenerator/chatRecord.db')
             cur = self.__conn.cursor()
             cur.execute(
                 """CREATE TABLE IF NOT EXISTS `chatrecord` (
