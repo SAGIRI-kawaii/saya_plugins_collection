87af06edfa6e0bed15c6a684a5481620a17a95d7
eeehhheee <64298630+eeehhheee@users.noreply.github.com>
2021-08-20 22:39:22 +0800

    修改readme并优化代码

--- modules/NiBuNengXXMa/__init__.py
+++ modules/NiBuNengXXMa/__init__.py
@@ -1,7 +1,10 @@
-# graia有关模块
+import logging
+import os
+
 from graia.application import GraiaMiraiApplication
+from graia.application.exceptions import AccountMuted
 from graia.application.message.chain import MessageChain
-from graia.application.message.elements.internal import Plain, At, Image, Source
+from graia.application.message.elements.internal import At, Image, Source
 from graia.application.event.messages import GroupMessage
 from graia.application.message.parser.kanata import Kanata
 from graia.application.message.parser.signature import RegexMatch
@@ -10,21 +13,14 @@ from graia.saya import Saya, Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.application.event.mirai import *
 
-from modules.NiBuNengXXMa import utils
-
-# 其他模块
-import random
-import aiohttp
-import json
-from io import BytesIO
+from PIL import ImageDraw, ImageFont
 
 from PIL import Image as IMG
 
-# 插件信息
 __name__ = "NiBuNengXXMa"
-__description__ = "生成 你不能xx吗 xx人 这种图"
+__description__ = "生成示例的这种图片"
 __author__ = "eeehhheee"
-__usage__ = "发送搜索 nbnxxm 文本1 文本2 即可"
+__usage__ = "发送nbnxxm 文本1 文本2 即可"
 
 saya = Saya.current()
 channel = Channel.current()
@@ -41,5 +37,37 @@ saya.module_context()
 @channel.use(ListenerSchema(listening_events=[GroupMessage], inline_dispatchers=[Kanata([RegexMatch('nbnxxm .* .*')])]))
 async def send_img(app: GraiaMiraiApplication, group: Group, member: Member, message: MessageChain, source: Source):
     _, text1, text2 = message.asDisplay().split(" ")
-    await app.sendGroupMessage(group, MessageChain.create(
-        [At(member.id), Image.fromLocalFile(utils.create_img(text1, text2))]), quote=source.id)
+    if os.path.exists(f'./modules/NiBuNengXXMa/temp/{text1} {text2}.jpg'):
+        try:
+            await app.sendGroupMessage(group, MessageChain.create(
+                [At(member.id), Image.fromLocalFile(f'./modules/NiBuNengXXMa/temp/{text1} {text2}.jpg')]),
+                                       quote=source.id)
+        except AccountMuted:
+            logging.warning('账户被禁言!')
+    else:
+        try:
+            res = await create_img(text1=text1, text2=text2)
+            await app.sendGroupMessage(group, MessageChain.create(
+                [At(member.id), Image.fromLocalFile(res)]), quote=source.id)
+        except AccountMuted:
+            logging.warning('账户被禁言!')
+
+
+font_size = 15
+
+
+async def create_img(text1: str, text2: str):
+    font = ImageFont.truetype('./modules/NiBuNengXXMa/ArialEnUnicodeBold.ttf', font_size)
+    font_width_text1, font_height_text1 = font.getsize(text1)
+    font_width_text2, font_height_text2 = font.getsize(text2)
+    font_width_text1 = font_width_text1 / 2
+    font_height_text1 = font_height_text1 / 2
+    font_height_text2 = font_height_text2 / 2
+    font_width_text2 = font_width_text2 / 2
+    img = IMG.open('./modules/NiBuNengXXMa/BasicImage.jpg')
+    draw = ImageDraw.Draw(img)
+    draw.text((225 - font_width_text1, 75 - font_height_text1), text=text1, fill=(0, 0, 0), font=font)
+    draw.text((350 - font_width_text2, 260 - font_height_text2), text=text2, fill=(0, 0, 0), font=font)
+    img_name = f'{text1} {text2}.jpg'
+    img.save(f'./modules/NiBuNengXXMa/temp/{img_name}')
+    return f'./modules/NiBuNengXXMa/temp/{img_name}'
